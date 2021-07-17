1d7abf92796e35eb81dfa67801243aa40d9b54fe
Manuel Valentino <manuelvicente67@hotmail.com>
2021-07-17 16:23:35 -0500

    fix config dialect 5

--- models/index.js
+++ models/index.js
@@ -5,7 +5,11 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, config);
+var sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD,
+    {
+        host: '',
+        dialect: 'mysql',
+    });
 var db = {};
 
 fs
@@ -18,8 +22,8 @@ fs
         db[model.name] = model;
     });
 
-Object.keys(db).forEach(function(modelName){
-    if ("associate" in db[modelName]){
+Object.keys(db).forEach(function (modelName) {
+    if ("associate" in db[modelName]) {
         db[modelName].associate(db);
     }
 });
