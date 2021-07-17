0edbdd3349fc46bf5d70548c4cce9245d80c03d8
Manuel Valentino <manuelvicente67@hotmail.com>
2021-07-17 15:54:59 -0500

    fix config db

--- config/config.js
+++ config/config.js
@@ -2,17 +2,17 @@ require('dotenv').config(); // this is important!
 module.exports = {
 
     "development": {
-        "username": "m322qbj5apwckt2p",
-        "password": "lin2bj1s9xvic84w",
-        "database": "fjuk1f0fa0hs92wj",
-        "host": "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        "DB_USERNAME": "m322qbj5apwckt2p",
+        "DB_PASSWORD": "lin2bj1s9xvic84w",
+        "DB_DATABASE": "fjuk1f0fa0hs92wj",
+        "DB_HOST": "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         "dialect": "mysql"
     },
     "staging": {
-        "username": "m322qbj5apwckt2p",
-        "password": "lin2bj1s9xvic84w",
-        "database": "fjuk1f0fa0hs92wj",
-        "host": "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        "DB_USERNAME": "m322qbj5apwckt2p",
+        "DB_PASSWORD": "lin2bj1s9xvic84w",
+        "DB_DATABASE": "fjuk1f0fa0hs92wj",
+        "DB_HOST": "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         "dialect": "mysql"
     }
 
