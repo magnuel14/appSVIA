f79fc024bfcdd2996b2694b10bfc86a8dd6f7de7
Manuel Valentino <manuelvicente67@hotmail.com>
2021-07-17 16:03:18 -0500

    fix config dialect 1

--- config/config.js
+++ config/config.js
@@ -6,14 +6,14 @@ module.exports = {
         "DB_PASSWORD": "lin2bj1s9xvic84w",
         "DB_DATABASE": "fjuk1f0fa0hs92wj",
         "DB_HOST": "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        "DB_DIALECT": "mysql"
+        "dialect": "mysql"
     },
     "staging": {
         "DB_USERNAME": "m322qbj5apwckt2p",
         "DB_PASSWORD": "lin2bj1s9xvic84w",
         "DB_DATABASE": "fjuk1f0fa0hs92wj",
         "DB_HOST": "ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        "DB_DIALECT": "mysql"
+        "dialect": "mysql"
     }
 
 
